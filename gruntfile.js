3244155035ef1585fd582e2b007e2cc83bfc5960
harrier2020 <ckfund90-sw@yahoo.com>
2021-10-28 17:47:11 +0800

    Grunt Part 1

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+  // Define the configuration for all the tasks
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            options:{
+                implementation:sass
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
+        },
+        
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
+    });
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);    
+};
